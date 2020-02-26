3499ea463dcd19a7ef6b917f105cb951c4b29896
peke-pon <toyo718@gmail.com>
2020-02-26 21:29:03 +0900

    jsonp

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ Vue.filter("numberFormat", function(val) {
 const app = new Vue({
   el: "#app",
   data: {
-    // 商品リスト
+    // 商品数
     count: 0,
     // 標準=1,安い順=2
     sortOrder: 1,
@@ -14,116 +14,24 @@ const app = new Vue({
     // 送料無料チェックボックス
     showDelvFree: false,
     // 商品リスト
-    products: [
-      {
-        id: 1,
-        name: "Spoon / <br>スマホケース",
-        price: 1380,
-        image:
-          "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
-        delv: 0,
-        isSale: true
-      },
-      {
-        id: 2,
-        name: "Spoon / <br>スマホケース",
-        price: 2480,
-        image:
-          "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
-        delv: 0,
-        isSale: false
-      },
-      {
-        id: 3,
-        name: "Spoon / <br>スマホケース",
-        price: 1440,
-        image:
-          "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
-        delv: 360,
-        isSale: true
-      },
-      {
-        id: 4,
-        name: "Spoon / <br>スマホケース",
-        price: 1450,
-        image:
-          "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
-        delv: 0,
-        isSale: false
-      },
-      {
-        id: 5,
-        name: "Spoon / <br>スマホケース",
-        price: 1280,
-        image:
-          "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
-        delv: 360,
-        isSale: true
-      },
-      {
-        id: 6,
-        name: "Spoon / <br>スマホケース",
-        price: 1320,
-        image:
-          "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
-        delv: 0,
-        isSale: true
-      },
-      {
-        id: 7,
-        name: "Spoon / <br>スマホケース",
-        price: 1380,
-        image:
-          "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
-        delv: 0,
-        isSale: true
-      },
-      {
-        id: 8,
-        name: "Spoon / <br>スマホケース",
-        price: 2480,
-        image:
-          "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
-        delv: 0,
-        isSale: false
-      },
-      {
-        id: 9,
-        name: "Spoon / <br>スマホケース",
-        price: 1440,
-        image:
-          "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
-        delv: 360,
-        isSale: true
-      },
-      {
-        id: 10,
-        name: "Spoon / <br>スマホケース",
-        price: 1450,
-        image:
-          "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
-        delv: 0,
-        isSale: false
-      },
-      {
-        id: 11,
-        name: "Spoon / <br>スマホケース",
-        price: 1280,
-        image:
-          "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
-        delv: 0,
-        isSale: true
-      },
-      {
-        id: 12,
-        name: "Spoon / <br>スマホケース",
-        price: 1320,
-        image:
-          "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
-        delv: 0,
-        isSale: true
-      }
-    ]
+    products: [],
+    // jsonpエラー
+    jsonError: false
+  },
+  created: function() {
+    $.ajax({
+      url: "http://localhost:8888/products.js",
+      type: "GET",
+      dataType: "jsonp",
+      jsonp: "callback",
+      jsonpCallback: "products"
+    })
+      .done((data, status, XHR) => {
+        this.products = data;
+      })
+      .fail((XHR, Status, error) => {
+        this.jsonError = true
+      })
   },
   computed: {
     filterdList: function() {
@@ -139,7 +47,6 @@ const app = new Vue({
         if (isShow) {
           newList.push(this.products[i]);
         }
-        // sorting
         if (this.sortOrder == 1) {
         } else if (this.sortOrder == 2) {
           newList.sort(function(x, y) {
---
+++ products.js
@@ -0,0 +1,98 @@
+products([
+  {
+        "id": 1,
+        "name": "Spoon / <br>スマホケース",
+        "price": 1380,
+        "image": "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
+        "delv": 0,
+        "isSale": true
+  },
+  {
+        "id": 2,
+        "name": "Spoon / <br>スマホケース",
+        "price": 2480,
+        "image": "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
+        "delv": 0,
+        "isSale": false
+  },
+  {
+        "id": 3,
+        "name": "Spoon / <br>スマホケース",
+        "price": 1440,
+        "image": "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
+        "delv": 360,
+        "isSale": true
+  },
+  {
+        "id": 4,
+        "name": "Spoon / <br>スマホケース",
+        "price": 1450,
+        "image": "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
+        "delv": 0,
+        "isSale": false
+  },
+  {
+        "id": 5,
+        "name": "Spoon / <br>スマホケース",
+        "price": 1280,
+        "image": "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
+        "delv": 360,
+        "isSale": true
+  },
+  {
+        "id": 6,
+        "name": "Spoon / <br>スマホケース",
+        "price": 1320,
+        "image": "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
+        "delv": 0,
+        "isSale": true
+  },
+  {
+        "id": 7,
+        "name": "Spoon / <br>スマホケース",
+        "price": 1380,
+        "image": "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
+        "delv": 0,
+        "isSale": true
+  },
+  {
+        "id": 8,
+        "name": "Spoon / <br>スマホケース",
+        "price": 2480,
+        "image": "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
+        "delv": 0,
+        "isSale": false
+  },
+  {
+        "id": 9,
+        "name": "Spoon / <br>スマホケース",
+        "price": 1440,
+        "image": "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
+        "delv": 360,
+        "isSale": true
+  },
+  {
+        "id": 10,
+        "name": "Spoon / <br>スマホケース",
+        "price": 1450,
+        "image": "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
+        "delv": 0,
+        "isSale": false
+  },
+  {
+        "id": 11,
+        "name": "Spoon / <br>スマホケース",
+        "price": 1280,
+        "image": "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
+        "delv": 0,
+        "isSale": true
+  },
+  {
+        "id": 12,
+        "name": "Spoon / <br>スマホケース",
+        "price": 1320,
+        "image": "http://placehold.jp/ffffff/afc3d6/220x300.png?text=%E5%95%86%E5%93%81%E7%94%BB%E5%83%8F",
+        "delv": 0,
+        "isSale": true
+  }
+]);
